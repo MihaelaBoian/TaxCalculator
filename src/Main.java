c1eaf5fda40c72b86bd98ddd230f841f9d51fad6
MihaelaBoian <pinzari_m@mail.ru>
2023-09-28 15:43:51 +0300

    n

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+       double price = 50.95 ;
+
+       double tax = 10.45 ;
+
+       int quantity = 100;
+
+       double total = 0;
+
+       String message= "I want to by " +quantity+" shirt!";
+
+       total = price*quantity*tax;
+
+        System.out.println(message);
+
+        System.out.println("Total cost with tax is " + total);
+
     }
 }
